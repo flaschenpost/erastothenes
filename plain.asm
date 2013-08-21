afad910e81c200512a958a6e5a0fa5e4ad63ec2b
flaschenpost <marco.gergele@gmail.com>
2013-08-21 08:44:41 +0200

    timing base - plain era sieve in asm

--- plain.asm
+++ plain.asm
@@ -1,4 +1,5 @@
 
+
 .X64
 .xmm
 
@@ -6,108 +7,177 @@ include write.inc
 stdout    equ 1
 SYS_EXIT  equ 1
 SYS_WRITE equ 4
-SYS_BRK   equ 12   ; 45 for int80
-pot2      equ 25
+SYS_BRK   equ 12
+POT2      equ 29
 
 
-.data
+    .data
+fmthex db 10, "hello %x ", 10, 0
+
+fmtprim db "% 5d %09d ",10, 0
+
 nonprim dq 0
 endmem dq 0
 tmp dq 0
+st_chk db 10,'c'
+st_prm db ' p'
 
+sttest db 10, "., anybody?0", 10
+stlen dd $ - sttest
 outbuf   dq 1024 DUP(0)
 
-.data?
+ .data?
 bits     dq 128 DUP(?)
 
-.code
+    .code
 
-breakme proc
-ret
-breakme endp
+    EXTRN printf:NEAR
+
+    breakme proc
+    ret
+    breakme endp
 
 main:
 
     ; alloc mem
     mov rax, SYS_BRK
-    mov rdi, 0
+    xor rdi, rdi
     syscall
 
     mov nonprim, rax
 
     mov rdi, 1
-    shl rdi, pot2 - 3
-    add rdi, rax
-    add rdi, 16
-
-    mov tmp, rbx
+    shl rdi, POT2-3
+    add rdi, nonprim
 
     mov rax, SYS_BRK
     syscall
-    sub rax, 16
+    sub rax, 8
     mov endmem, rax
 
 
-    ; which bit?
+    ; the counter for the number of prime
+    xor r14, r14
+    add r14,1
+    
+    ; prime number 2 is first
+    ;; xor rax,rax
+    ;; mov rdi, offset fmtprim
+    ;; mov rdx, 2
+    ;; mov rsi, r14
+    ;; call printf
 
-    ; bit-position 
+
     xor r8,r8
     sub r8,1
-    
+
     outer_loop:
 
     add r8,1
 
     ; extract byte- and bit-number
-    
+    ; r10 = byte number (increment by 8 for 64-bit)
+    ; rbx is bit-position in byte
     mov r10,r8
-    mov rax,r8
-    shr r10, 6
-    and rax, 63
+    mov rbx,r8
+    shr r10, 3
+    and r10,-8
+    and rbx, 63
 
+    ; now r10 is memory-adress
     add r10, nonprim
 
     cmp r10, endmem
     jge outer_break
-    ; extract byte- and bit-number
-    bt [r10], ax
+
+    ; test bit - if set, the number is composite
+    bt [r10], bx
     jc outer_loop
 
-    push r10
+    ; still here? So next prime number
+    add r14,1
+
+    ; position in bitstream
     push r8
 
-    ; here is r8 a prime bit
-    ; and r9 is the prime number itself
-    shl r8,1
-    add r8,3
+    ; r9 gets the prime number itself
+    mov r9,r8
+    shl r9,1
+    add r9,3
+
+    ; output p
+    ;; push rbx
+    ;; push r10
+    ;; push r9
+    ;; push r8
+    ;; xor rax,rax
+    ;; mov rdi, offset fmtprim
+    ;; mov rdx, r9
+    ;; mov rsi, r14
+    ;; call printf
+    ;; pop r8
+    ;; pop r9
+    ;; pop r10
+    ;; pop rbx
+    ; end of output p
+
+    ; to first relevant bit - square
+    mov rax, r9
+    imul rax, rax
+    jc outer_break
+    shr rax, 1
+    sub rax,1
+    mov rbx, rax
+
+    ; to first relevant bit - simple
+    ;; add rbx, r9
+    ;; mov rax, rbx
+
+    ; split starting bit
+    shr rax, 3
+    and rax, -8
+    and rbx,63
+    add r10, rax
 
     mov rcx, endmem
     sub rcx, r10
+    jc inner_break
+    jz inner_break
+
     sub rcx,1
     inner_loop:
-        add rax, r8
-        mov rbx, rax
-        shr rbx, 3
-        and rbx, -8
-        add r10, rbx
-        sub rcx, rbx
-        jc inner_break
-        and rax,63
+        bts [r10], bx
 
-        bts [r10], ax
+        add rbx, r9
+        mov rax, rbx
+        shr rax, 3
+        and rax, -8
+        and rbx,63
+        add r10, rax
+        sub rcx, rax
+        jc inner_break
+
     jmp inner_loop
 
     inner_break:
+    ;; call write
 
     pop r8
-    pop r10
-
-    cmp r10, endmem
-    jl outer_loop
+    jmp outer_loop
 
     outer_break:
 
     call write 
+
+    ; mov rax, nonprim
+    ; mov [rax], r8
+    ; mov [rax+1], r9
+    ; mov [rax+2], r10
+
+    ; call write 
+    ; call write 
+
+    jmp end_prog
 
     end_prog:
 
